c24cf191a8b9da9ca49ebc63871d1a18a097c229
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-11-30 22:14:44 -0200

    Add ColumnList e salvando items em localStorage

--- src/App.js
+++ src/App.js
@@ -2,20 +2,92 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ColumnList from './ColumnList';
+
 class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      items: []
+    }
+  }
+
+  componentWillMount(){
+    const items = window.localStorage.getItem('toDoListItems') || '[]';
+    this.setState({items: JSON.parse(items)});
+  }
+
+  updateLocalStorage (items){
+    window.localStorage.setItem('toDoListItems', JSON.stringify(items))
+  }
+
+  addTask = (e) => {
+    e.preventDefault();
+
+    const { target } = e;
+    const input = target.querySelector("input") || {};
+    const { value = "" } = input;
+
+    this.setState(function (prev) {
+      const { items } = prev;
+
+      items.push({
+        id: items.length + 1,
+        title: value,
+        status: "To do"
+      })
+
+      this.updateLocalStorage(items);
+
+      return { items };
+    })
+  }
+
+  updateTask = (target, task) => {
+    this.setState(prev => {
+      const { items = [] } = prev;
+
+      const s = items.filter(item => item.id !== task.id);
+
+      task.status = target.checked ? "Done" : "To do";
+
+      s.push(task)
+
+      this.updateLocalStorage(s);
+
+      return { items: s };
+    })
+  }
+
   render() {
+
+    const { items } = this.state;
+    const columns = [
+      { items, title: "To do" },
+      { items, title: "Done" }
+    ];
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="app-lists">
+
+          {columns.map(column => (
+            <ColumnList
+              key={column.title}
+              title={column.title}
+              items={column.items}
+              addTask={this.addTask}
+              updateTask={this.updateTask} />
+          ))}
+
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App
---
+++ src/ColumnList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import './ColumnList.css';
+import If from './If';
+
+const ColumnList = ({ title, items = [], addTask, updateTask }) => {
+
+    const currentTasks = items.filter(item => (item.status === title))
+
+    return (
+        <div className="column-list">
+            <h3>{title}</h3>
+            <If test={title === "To do"} >
+                <form onSubmit={addTask}>
+                    <input type="text" placeholder="Create new Task" />
+                    <button type="submit">
+                        Create
+                    </button>
+                </form>
+            </If>
+            <ul className="list-items">
+                {currentTasks.map(item => (
+                    <li key={item.id}>
+                        <input type="checkbox"
+                            onChange={(e) => updateTask(e.target, item)}
+                            checked={item.status === "Done"}
+                        />
+                        <span>{item.title}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default ColumnList;
